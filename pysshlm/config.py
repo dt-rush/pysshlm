eac9f86ae95836d52de1bdb15da6d7f6275591a9
dt-rush <nick.8.payne@gmail.com>
2018-07-09 12:32:13 -0400

    added .pytest_cache/ to .gitignore and removed blank lines at end of config.py (flake8)

--- pysshlm/config.py
+++ pysshlm/config.py
@@ -7,5 +7,3 @@ here = path.abspath (path.dirname (__file__))
 configparser = SafeConfigParser()
 configparser.read (path.join (here, "pysshlm.cfg"))
 pysshlm_config = configparser._sections['pysshlm']
-
-
